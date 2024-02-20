2f33680268eafd6072d37cff8a8399aa18adb014
rossjohneast@hotmail.com <rossjohneast@hotmail.com>
2024-02-20 14:30:37 +0000

    Allow all block types in advanced carousel

--- src/components/carousel-swiper-item/edit.js
+++ src/components/carousel-swiper-item/edit.js
@@ -10,15 +10,7 @@ export default function Edit() {
 	return (
 		<div {...useBlockProps()}>
 			<InnerBlocks
-				allowedBlocks={[
-					'zenflow5/section', 
-					'zenflow5/container', 
-					'zenflow5/row', 
-					'zenflow5/col', 
-					'zenflow5/card', 
-					'core/image',
-					'core/cover'
-				]}
+
 				template={[
 					['core/image']
 				]}
